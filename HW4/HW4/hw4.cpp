72d30dbceabec51cebdb7c71e57be026299f2c5a
jqzhang <billjqzhang@outlook.com>
2018-05-06 16:21:08 +0800

    hw4

--- HW4/HW4/hw4.cpp
+++ HW4/HW4/hw4.cpp
@@ -2,13 +2,37 @@
 #include<stdio.h>
 #include<iostream>
 #include<fstream>
+#include<algorithm>
 
 using namespace std;
+
+int greedy(int)
+{
+	return 0;
+}
+bool complare(int a, int b)
+{
+	return a>b;
+}
+
 int main()
 {
 	int n;
 	cin >> n;
-	int a;
+	double p[1000];
+	double sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> p[i];
+		sum += p[i];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		p[i] = p[i] / sum;
+		//cout << p[i];
+	}
+	sort(p, p + n, complare);
 
 	system("pause");
 	return 0;
